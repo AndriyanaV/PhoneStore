bb690387e11ea423065913dbdc02ad08e18a5c75
Andrijana <andrijanavladimirov5@gmail.com>
2024-11-21 14:55:40 +0100

    work on desing at homepage

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,106 +1,80 @@
-const passwordField=document.getElementById("password");
-const emailField=document.getElementById("email");
+const passwordField = document.getElementById("password");
+const emailField = document.getElementById("email");
 
-function validation (event){
+function validation(event) {
   event.preventDefault();
-  const email=document.getElementById("email").value;
-  const passwordError=document.getElementById("passwordError");
-  const mailError=document.getElementById("mailError");
-  const errorOutline=document.querySelectorAll("input");
-  const password=document.getElementById("password").value;
-  const eyeButton=document.getElementById("eye-button");
-  
+  const email = document.getElementById("email").value;
+  const passwordError = document.getElementById("passwordError");
+  const mailError = document.getElementById("mailError");
+  const errorOutline = document.querySelectorAll("input");
+  const password = document.getElementById("password").value;
+  const eyeButton = document.getElementById("eye-button");
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  passwordError.style.visibility= 'hidden';
-  mailError.style.visibility= 'hidden';
-  errorOutline[0].style.borderColor= 'black';
-  errorOutline[1].style.borderColor= 'black';
-  
-  
+  passwordError.style.visibility = "hidden";
+  mailError.style.visibility = "hidden";
+  errorOutline[0].style.borderColor = "black";
+  errorOutline[1].style.borderColor = "black";
 
-  function oneUpperCase(password){
-    const regexUpperCase=/[A-Z]/;
+  function oneUpperCase(password) {
+    const regexUpperCase = /[A-Z]/;
     return regexUpperCase.test(password);
   }
 
-  function numberIn(password){
-    const regexNumberIn=/.*[1-9].*/;
+  function numberIn(password) {
+    const regexNumberIn = /.*[1-9].*/;
     return regexNumberIn.test(password);
   }
 
-
-
-
-  function errorInMail(txt){
-    mailError.style.visibility='visible';
-    mailError.innerText=txt;
-    emailField.style.border="1px solid red"
-    
-
+  function errorInMail(txt) {
+    mailError.style.visibility = "visible";
+    mailError.innerText = txt;
+    emailField.style.border = "1px solid red";
   }
 
-
-  function errorInPassword(txt){
-    passwordError.style.visibility='visible';
-    passwordError.innerText=txt;
-    passwordField.style.border="1px solid red";
-    eyeButton.style.border="1px solid red";
-    eyeButton.style.borderLeft="none";
-
+  function errorInPassword(txt) {
+    passwordError.style.visibility = "visible";
+    passwordError.innerText = txt;
+    passwordField.style.border = "1px solid red";
+    eyeButton.style.border = "1px solid red";
+    eyeButton.style.borderLeft = "none";
   }
 
-
-  
-
-
-  
-  if (email===""){
+  if (email === "") {
     errorInMail("Please enter your email adress!");
     return;
   }
   if (!emailRegex.test(email)) {
-        errorInMail("Your email address is not in the correct format!");
-        return;
+    errorInMail("Your email address is not in the correct format!");
+    return;
   }
-  
 
-
-  if(password===""){
+  if (password === "") {
     errorInPassword("Please enter your password!");
     return;
   }
-  if(password.length<3){
+  if (password.length < 3) {
     errorInPassword("The password must have a minimum of three characters!");
     return;
   }
-  if(!oneUpperCase(password)){
-      errorInPassword("The password must contain at least one uppercase letter!");
-      return;
-    }
-  if(!numberIn(password)){
-        errorInPassword("The password must contain at least one number!");
-        return;
-    }
+  if (!oneUpperCase(password)) {
+    errorInPassword("The password must contain at least one uppercase letter!");
+    return;
+  }
+  if (!numberIn(password)) {
+    errorInPassword("The password must contain at least one number!");
+    return;
+  }
 
-  
-  window.location.href="/pages/homepage";
-    
-  
-
-
-
-
+  window.location.href = "/pages/homepage";
 }
 
-
-function seePassword(event){
+function seePassword(event) {
   event.preventDefault();
 
-  if(passwordField.type==="password"){
-    passwordField.type="text";
-  }
-  else{
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+  } else {
     passwordField.type = "password";
   }
 }
