dab918164298c35f3644513bd38a7edc238db8af
Andrijana <andrijanavladimirov5@gmail.com>
2024-11-26 23:31:06 +0100

    create a functions for sliders

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -127,95 +127,93 @@ const teamMembers = [
     }
   ];
 
-  n=3
-  perosnas=[]
-  function moveToRight() {
-    // Clear `perosnas` once at the start
-    perosnas.length = 0;
-
-    // Get the next 3 members
-    for (let i = 0; i < 3; i++) {
-        
-        if (n === teamMembers.length) {
-            n = 0;
-        }
-
-        
-        perosnas.push(teamMembers[n]);
-        n++;
-    }
-
-    // Log to verify the current set of members
   
-    setMembers(perosnas)
-}
 
-p=teamMembers.length-3
-arrLeft=[]
+let rightIndex=3
+let leftIndex=teamMembers.length-1
+let persons=[]
 
-if(n!=3 ){
-    p=n-6
- }
 
-function moveToLeft(){
-
-    arrLeft=[] 
-
-    if(p<0){
-        p=6
-    }
-    
+function moveToSlider(direction){
+  if(direction=='right'){
    
 
-    
-    for (let i = 0; i < 3; i++) {
-        arrLeft.push(teamMembers[p]);
-        console.log(p)
-        p++;
+    for(i=0;i<3;i++){
+      if(rightIndex>teamMembers.length-1){
+        rightIndex=0
+      }
+
+      persons.push(teamMembers[rightIndex])
+      rightIndex++
     }
+    setMembers(persons)
+    persons=[]
+    leftIndex=rightIndex-4
+  }
 
+  else{
     
-    p-=6
-
+    for(i=0;i<3;i++){
+      if(leftIndex<0){
+        leftIndex=teamMembers.length-1
+      }
+      persons.push(teamMembers[leftIndex])
+      leftIndex--
+    }
     
-
-    setMembers(arrLeft)
-    
+    rightIndex=leftIndex+4
+    reversedArray=persons.reverse()
+    setMembers(reversedArray)
+    persons=[]
+  }
 
 }
 
  let startAtPhone=1
  let endAtPhone=8
 
-function moveToRightMobile(){
-  
-  if(startAtPhone== teamMembers.length){
-    startAtPhone=0
+function moveToSliderMobile(direction){
+
+  if(direction=="right"){
+    if(startAtPhone>teamMembers.length-1){
+      startAtPhone=0
+    }
+
+    setUpMobile(startAtPhone)
+    startAtPhone++
+    endAtPhone=startAtPhone-2
   }
 
+  else{
+    if(endAtPhone<0){
+      endAtPhone=8
+    }
+
+    setUpMobile(endAtPhone)
+    endAtPhone--
+    startAtPhone=endAtPhone+2
+    
+  }
+  
+  
+
+  
+}
+
+function setUpMobile(index){
   let sliderImage = document.querySelectorAll(".person-image-mobile");
   let sliderLinkedin = document.querySelectorAll(".linkedin-profile-mobile");
   let sliderName = document.querySelectorAll(".name-mobile");
   let sliderPosition = document.querySelectorAll(".position-mobile");
 
-  sliderImage[0].src=teamMembers[startAtPhone].img
-  sliderName[0].innerHTML=teamMembers[startAtPhone].name
-  sliderPosition[0].innerHTML=teamMembers[startAtPhone].position
-
+  sliderImage[0].src=teamMembers[index].img
+  sliderName[0].innerHTML=teamMembers[index].name
+  sliderPosition[0].innerHTML=teamMembers[index].position
   
-  startAtPhone++ 
-  decreaseCounterByTwo()
 }
 
-function decreaseCounterByTwo() {
-   if(startAtPhone!=1){
-     endAtPhone=startAtPhone-2
-   }
 
-   else{
-     endAtPhone=teamMembers.length-1
-   }
-}
+
  
 
 
